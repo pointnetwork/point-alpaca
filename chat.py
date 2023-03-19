5a5a3a38dcb76888322eaf5d2c4fea35b9b96952
Valentin De Matos <43698357+Thytu@users.noreply.github.com>
2023-03-19 19:55:26 +0100

    refactor(chat): clean the demo app

--- chat.py
+++ chat.py
@@ -1,20 +1,9 @@
-import os, json, itertools, bisect, gc
-
-from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
-import transformers
 import torch
-from accelerate import Accelerator
-import accelerate
-import time
+import transformers
 
-model = None
-tokenizer = None
-generator = None
 
-def load_model(model_name, eight_bit=0, device_map="auto"):
+def load_model(path: str, eight_bit: bool = False, device_map="auto"):
     global model, tokenizer, generator
-
-    print("Loading "+model_name+"...")
 
     if device_map == "zero":
         device_map = "balanced_low_0"
@@ -23,46 +12,37 @@ def load_model(model_name, eight_bit=0, device_map="auto"):
     gpu_count = torch.cuda.device_count()
     print('gpu_count', gpu_count)
 
-    tokenizer = transformers.LLaMATokenizer.from_pretrained(model_name)
+    tokenizer = transformers.LLaMATokenizer.from_pretrained(path)
     model = transformers.LLaMAForCausalLM.from_pretrained(
-        model_name,
-        #device_map=device_map,
-        #device_map="auto",
+        path,
+        device_map=device_map,
         torch_dtype=torch.float16,
-        #max_memory = {0: "14GB", 1: "14GB", 2: "14GB", 3: "14GB",4: "14GB",5: "14GB",6: "14GB",7: "14GB"},
-        #load_in_8bit=eight_bit,
+        # max_memory = {0: "14GB", 1: "14GB", 2: "14GB", 3: "14GB",4: "14GB",5: "14GB",6: "14GB", 7: "14GB"},
+        load_in_8bit=eight_bit,
         low_cpu_mem_usage=True,
-        load_in_8bit=False,
         cache_dir="cache"
-    ).cuda()
+    )
 
-    generator = model.generate
+    return model.generate, tokenizer
 
-load_model("./result")
 
-history = []
+def chat_with_alpaca(model_generator, tokenizer):
 
-def go():
-    invitation = "Assistant: "
-    human_invitation = "Human: "
+    history = []
 
-    # input
-    msg = input(human_invitation)
-    print("")
-
-    history.append(human_invitation + msg)
-
-    fulltext = "\n\n".join(history) + "\n\n" + invitation
-
-#    print('SENDING==========')
-#    print(fulltext)
-#    print('==========')
-
-    generated_text = ""
-    gen_in = tokenizer(fulltext, return_tensors="pt").input_ids.cuda()
-    in_tokens = len(gen_in)
     with torch.no_grad():
-            generated_ids = generator(
+
+        while True:
+
+            user_input = input("Human: ")
+
+            history.append("Human: " + user_input)
+
+            model_input = "\n\n".join(history) + "\n\nAssistant: "
+
+            gen_in = tokenizer(model_input, return_tensors="pt").input_ids.cuda()
+
+            generated_ids = model_generator(
                 gen_in,
                 max_new_tokens=200,
                 use_cache=True,
@@ -70,26 +50,24 @@ def go():
                 num_return_sequences=1,
                 do_sample=True,
                 repetition_penalty=1.1, # 1.0 means 'off'. unfortunately if we penalize it it will not output Sphynx:
-                temperature=0.5, # default: 1.0
-                top_k = 50, # default: 50
-                top_p = 1.0, # default: 1.0
+                temperature=0.5,        # default: 1.0
+                top_k = 50,             # default: 50
+                top_p = 1.0,            # default: 1.0
                 early_stopping=True,
             )
-            generated_text = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0] # for some reason, batch_decode returns an array of one element?
 
-            text_without_prompt = generated_text[len(fulltext):]
+            generated_text = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
 
-    response = text_without_prompt
+            response = generated_text[len(model_input):]
 
-    response = response.split(human_invitation)[0]
+            response = "Assistant:" + response.split(user_input)[0].strip()
 
-    response.strip()
+            print("\n", response, end="\n\n")
 
-    print(invitation + response)
+            history.append(response)
 
-    print("")
 
-    history.append(invitation + response)
+if __name__ == "__main__":
+    model_generator, tokenizer = load_model(path="./result")
 
-while True:
-    go()
+    chat_with_alpaca(model_generator, tokenizer)
